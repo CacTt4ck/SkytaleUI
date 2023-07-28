8554fd2afaa760ee538378ccbd993e3afdbd1f29
CacTt4ck <lachainedelightning13090@gmail.com>
2023-07-28 23:46:40 +0200

    Logo smooth

--- src/com/cactt4ck/sui/gui/PFrame.java
+++ src/com/cactt4ck/sui/gui/PFrame.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public final class PFrame extends JFrame {
 
-    private JPanel rootPanel, editionPanel;
+    private final JPanel rootPanel, editionPanel;
 
     public PFrame() {
         this.rootPanel = new PPanel(this);
@@ -15,7 +15,7 @@ public final class PFrame extends JFrame {
         this.setTitle("SkytaleUI");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setIconImage(new ImageIcon(getClass().getResource("/assets/images/logo.png")).getImage());
+        this.setIconImage(new ImageIcon(getClass().getResource("/assets/images/logo.png")).getImage().getScaledInstance(64,64, Image.SCALE_SMOOTH));
         this.setLocationRelativeTo(null);
         this.setContentPane(this.rootPanel);
         this.setVisible(true);
