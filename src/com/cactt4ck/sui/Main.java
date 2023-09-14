67fc26fec3a74af0b44060e5a188fd31012730c4
CacTt4ck <lachainedelightning13090@gmail.com>
2023-09-14 22:44:30 +0200

    config.properties

--- src/com/cactt4ck/sui/Main.java
+++ src/com/cactt4ck/sui/Main.java
@@ -3,16 +3,15 @@ package com.cactt4ck.sui;
 import com.cactt4ck.sui.gui.PFrame;
 import com.cactt4ck.sui.utils.AppConfig;
 import com.formdev.flatlaf.FlatDarculaLaf;
-import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
 
 public class Main {
-
     public static final AppConfig CONFIG = new AppConfig();
 
     public static void main(String[] args) {
+
         if (CONFIG.getProperty("theme").equalsIgnoreCase("dark"))
             FlatDarculaLaf.setup();
         else
